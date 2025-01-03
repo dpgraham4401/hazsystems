b0be4fa5d8f20c9f97e3f1e9a21b76035a304203
David Paul Graham <dpgraham4401@gmail.com>
2025-01-03 13:14:52 -0500

    add pytest and dummy test to show that migrations are being run

---
+++ esystem/tests/test_foo.py
@@ -0,0 +1,20 @@
+# Create your tests here.
+
+import pytest
+
+from esystem.models import HazardousWasteSystem
+
+
+@pytest.mark.django_db
+class TestDummyClass:
+
+    @pytest.fixture
+    def string(self):
+        return "hello"
+
+    def test_dummy(self, string):
+        assert string == "hello"
+
+    def test_create_model(self):
+        system = HazardousWasteSystem.objects.create(name="Test System")
+        assert isinstance(system, HazardousWasteSystem)
